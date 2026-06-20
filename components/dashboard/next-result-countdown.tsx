ca5b56144268d9a8a1fade6b5b1a94cddfe8f433
Lucas Berthet <lu.berthet@gmail.com>
2026-06-20 19:56:27 +0200

    add more info

--- components/dashboard/next-result-countdown.tsx
+++ components/dashboard/next-result-countdown.tsx
@@ -1,12 +1,23 @@
 "use client"
 
-import { Clock } from "lucide-react"
+import { Clock, Info } from "lucide-react"
 import { useTranslations } from "next-intl"
 import { useCountdownTo2AMParis } from "@/hooks/use-countdown-to-2am-paris"
+import { useGetPromptListSuspenseQuery } from "@/hooks/queries/useGetPromptList"
+import { Card } from "@/components/ui/card"
 
 export function NextResultCountdown() {
   const t = useTranslations("dashboard")
   const countdown = useCountdownTo2AMParis()
+  const { data: prompts } = useGetPromptListSuspenseQuery()
+
+  if (!prompts.length)
+    return (
+      <Card className="mt-4 flex flex-row p-4">
+        <Info className="mt-0.5 size-4" />
+        <p className="flex items-center font-bold">{t("addPrompt")}</p>
+      </Card>
+    )
 
   if (!countdown) return null
 
